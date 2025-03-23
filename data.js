ab8c71545d48559f1d770c8f22b87b709b0881b1
aysntltri <ayusintalestari26@gmail.com>
2025-03-23 20:56:19 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,15 @@
+const nama = "ayu sinta lestari";
+let usia = 16;
+
+function generateBiodta() {
+  if (usia) {
+    //INI ADALAH KONDISI PERTAMA
+  } else {
+    //INI ADALAH JIKA KONDISI TIDAK TERPENUHI
+  }
+  console.log(`nama saya adalah ${nama} dan usia saya ${usia}`);
+}
+
+console.log(nama);
+console.log(usia);
+generateBiodta();
